0bd149f9f6445d46774caa3afbd7623c6e18100f
Santosh Ashok Dhawade <dhawadesantosh9191@gmail.com>
2025-04-13 20:37:58 +0530

    initial commit

---
+++ Assignment_1_1.java
@@ -0,0 +1,34 @@
+// 1.Program to divide two numbers
+
+import java.util.*;
+
+class Div
+{
+	public int Divide(int iNo1, int iNo2)
+	{
+		int iAns =0 ;
+		if (iNo2<0)
+	{
+	return -1;
+	}
+		iAns = iNo1 / iNo2;
+		return iAns;
+	}
+}
+
+class Assignment_1_1
+
+{
+	public static void main(String args[])
+	{
+		int iValue1 = 15 , iValue2 = 25;
+		int iRet = 0;
+		
+		Div obj = new Div();
+		
+		iRet = obj.Divide(iValue1, iValue2);
+		
+		System.out.println("Division is:"+iRet);
+		
+	}
+}
