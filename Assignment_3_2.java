75befd0f46540d60b7e74b9ad7efe1d4579ca499
Santosh Dhawade <dhawadesantosh9191@gmail.com>
2025-05-01 19:21:04 +0530

    write a program hich accept number from user and print even factors of that number

---
+++ Assignment_3_2.java
@@ -0,0 +1,38 @@
+/*2. write a program hich accept number from user and
+print even factors of that number.
+
+Input  : 24
+Output : 2 4 6 8 12 */
+
+import java.util.*;
+
+class Demo{
+    public void Display(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1 ; iCnt <= iNo/2 ; iCnt++)
+        {
+            if(iNo % iCnt == 0 && iCnt % 2 == 0)
+            {
+                System.out.print(" "+iCnt);
+            }
+        }
+    }
+}
+
+class Assignment_3_2
+{
+    public static void main(String args[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the number");
+        iValue = sobj.nextInt();
+
+        Demo obj = new Demo();
+
+        obj.Display(iValue);
+    }
+}
